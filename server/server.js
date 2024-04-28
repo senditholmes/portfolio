c47931e44fc5b159a4d7c4ba42033b6721bcf0f9
Daniel Holmes <senditholmes@gmail.com>
2024-04-28 18:01:07 +0900

    connect sqlite3 database

--- server/server.js
+++ server/server.js
@@ -1,12 +1,25 @@
 import express from "express";
 import serveStatic from "serve-static";
+import sqlite3 from "sqlite3";
 const app = express();
 const port = 3000;
 app.use(serveStatic("/Users/djh/Projects/portfolio/public", {
     index: ["index.html", "index.htm"],
 }));
+const db = new sqlite3.Database(":memory:", (err) => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log("Connected to the in-memory SQlite database.");
+});
+db.close((err) => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log("Close the database connection.");
+});
 app.get("/test", (req, res) => {
-    res.send(`<h1> Here is your example HTML response buddy! </h1>`);
+    res.send(`Response goes here`);
 });
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import serveStatic from "serve-static";
+import sqlite3 from "sqlite3";
 
 const app = express();
 const port = 3000;
@@ -11,11 +12,26 @@ app.use(
   })
 );
 
+//DATABASE CONNECTION (MEMORY)
+const db = new sqlite3.Database(":memory:", (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log("Connected to the in-memory SQlite database.");
+});
+db.close((err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log("Close the database connection.");
+});
+
 // ROUTES
 app.get("/test", (req, res) => {
   res.send(`Response goes here`);
 });
 
+// LISTEN
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
