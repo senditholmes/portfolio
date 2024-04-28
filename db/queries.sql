421e4d13f882271355f3308e6a33ab929aab4d18
Daniel Holmes <senditholmes@gmail.com>
2024-04-28 19:36:21 +0900

    add database test query

---
+++ db/queries.sql
@@ -0,0 +1,9 @@
+/* CREATE TABLE portfolio (
+   id INT,
+   ProjectName TEXT,
+   Type TEXT,
+   PRIMARY KEY(id)
+); */
+
+/* INSERT INTO portfolio ( ProjectName, Type)
+VALUES( 'TESTPROJECT',	'TYPESCRIPT' ); */
--- server/server.js
+++ server/server.js
@@ -6,20 +6,27 @@ const port = 3000;
 app.use(serveStatic("/Users/djh/Projects/portfolio/public", {
     index: ["index.html", "index.htm"],
 }));
-const db = new sqlite3.Database(":memory:", (err) => {
-    if (err) {
-        return console.error(err.message);
-    }
-    console.log("Connected to the in-memory SQlite database.");
-});
-db.close((err) => {
-    if (err) {
-        return console.error(err.message);
-    }
-    console.log("Close the database connection.");
-});
-app.get("/test", (req, res) => {
-    res.send(`Response goes here`);
+app.get("/getProjects", (req, res) => {
+    const db = new sqlite3.Database("/Users/djh/Projects/portfolio/db/portfolio.db", (err) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        console.log("Connected to portfolio.db SQlite database.");
+    });
+    db.serialize(() => {
+        db.each(`SELECT * FROM portfolio`, (err, row) => {
+            if (err) {
+                console.error(err.message);
+            }
+            res.send(`<div> Project name is: ${row.ProjectName}</div>`);
+        });
+    });
+    db.close((err) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        console.log("Close the database connection.");
+    });
 });
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
