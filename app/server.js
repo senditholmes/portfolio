0726da0df6818ae1c2509679e656606494b2c9ff
Daniel Holmes <senditholmes@gmail.com>
2024-05-15 15:06:32 +0900

    add basic form logic, arrange templates

--- app/server.js
+++ app/server.js
@@ -6,8 +6,9 @@ const port = process.env.PORT;
 const octokit = new Octokit({
     auth: process.env.ACCESS_TOKEN,
 });
-app.use(express.static("./public"));
+app.use(express.static("public"));
 app.use(express.json());
+app.use(express.urlencoded());
 app.get("/home", (req, res) => {
     res.send(`<div> Hero </div>`);
 });
@@ -25,7 +26,7 @@ app.get("/projects", (req, res) => {
             res.send(`
       <ul>
         ${repos
-                .map((repo) => `<li>${repo.name}, ${repo.language}, ${repo.url}, ${repo.created_at} </li>`)
+                .map((repo) => `<li>${repo.name}, ${repo.language}, ${repo.url}, ${repo.created_at}</li>`)
                 .join("")}
       </ul>
       `);
@@ -38,62 +39,95 @@ app.get("/projects", (req, res) => {
     getRepos();
 });
 app.get("/about", (req, res) => {
-    res.send(`
-  <div class="flex flex-col">
-  <h1 class="pb-3">Daniel Holmes - Fullstack Engineer</h1>
-  <ul class="flex">
-    <li>
-      <h2>Frontend</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
-        molestias quam non deserunt atque inventore? Modi ducimus dolore placeat
-        reiciendis, accusamus eveniet alias obcaecati cum doloremque fuga, ut,
-        neque sunt!
-      </p>
-    </li>
-
-    <li>
-      <h2>Backend</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
-        molestias quam non deserunt atque inventore? Modi ducimus dolore placeat
-        reiciendis, accusamus eveniet alias obcaecati cum doloremque fuga, ut,
-        neque sunt!
-      </p>
-    </li>
-
-    <li>
-      <h2>Data Analysis and Database Management</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
-        molestias quam non deserunt atque inventore? Modi ducimus dolore placeat
-        reiciendis, accusamus eveniet alias obcaecati cum doloremque fuga, ut,
-        neque sunt!
-      </p>
-    </li>
-
-    <li>
-      <h2>Teaching Professional</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
-        molestias quam non deserunt atque inventore? Modi ducimus dolore placeat
-        reiciendis, accusamus eveniet alias obcaecati cum doloremque fuga, ut,
-        neque sunt!
-      </p>
-    </li>
-  </ul>
-</div>
-
-   
-  `);
+    res.send(about_template);
 });
 app.get("/contact", (req, res) => {
-    res.send(`<div> Contact </div>`);
+    res.send(contact_form);
+});
+app.post("/contact", (req, res) => {
+    const formUserInput = req.body;
+    res.send(form_submission_thankyou);
 });
 app.get("/downloadCv", (req, res) => {
-    res.download("app/public/assets/filesToSend/SoftwareCV.pdf");
+    res.download("./public/assets/filesToSend/SoftwareCV.pdf");
 });
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`Server listening on port ${port}`);
 });
+const about_template = `
+<div class="flex flex-col">
+<h1 class="pb-3">Daniel Holmes - Fullstack Engineer</h1>
+<ul class="flex">
+  <li>
+    <h2>Frontend</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+      molestias quam non deserunt atque inventore? Modi ducimus dolore placeat
+      reiciendis, accusamus eveniet alias obcaecati cum doloremque fuga, ut,
+      neque sunt!
+    </p>
+  </li>
+
+  <li>
+    <h2>Backend</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+      molestias quam non deserunt atque inventore? Modi ducimus dolore placeat
+      reiciendis, accusamus eveniet alias obcaecati cum doloremque fuga, ut,
+      neque sunt!
+    </p>
+  </li>
+
+  <li>
+    <h2>Data Analysis and Database Management</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+      molestias quam non deserunt atque inventore? Modi ducimus dolore placeat
+      reiciendis, accusamus eveniet alias obcaecati cum doloremque fuga, ut,
+      neque sunt!
+    </p>
+  </li>
+
+  <li>
+    <h2>Teaching Professional</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+      molestias quam non deserunt atque inventore? Modi ducimus dolore placeat
+      reiciendis, accusamus eveniet alias obcaecati cum doloremque fuga, ut,
+      neque sunt!
+    </p>
+  </li>
+</ul>
+</div>
+`;
+const contact_form = `<form hx-post="/contact" hx-trigger="submit" hx-target="#display-content" hx-swap="beforeend">
+<div>
+  <label for="name">Name</label>
+  <input name="name" type="text" id="name" required />
+</div>
+
+<div>
+  <label for="phone">Phone*</label>
+  <input name="phone" type="text" id="phone" />
+</div>
+
+<div>
+  <label name="email">Email</label>
+  <input name="email" type="text" id="email" required />
+</div>
+
+<div>
+  <label for="comment">Comment</label>
+  <input
+    name="comment"
+    type="text"
+    id="comment"
+    placeholder="Please tell me a little bit about your request."
+  />
+</div>
+
+<button type="submit">Request More info</button>
+</form>
+`;
+const form_submission_thankyou = `<div>Thanks for contacting me. I will get back to you asap!</div>`;
 //# sourceMappingURL=server.js.map
--- app/server.ts
+++ app/server.ts
@@ -16,15 +16,25 @@ interface Repo {
   language: string;
 }
 
+interface FormSchema {
+  name: string;
+  phone: string | "";
+  email: string;
+  comment: string | "";
+}
+
 // MIDDLEWARE
-app.use(express.static("./public"));
+app.use(express.static("public"));
 app.use(express.json());
+app.use(express.urlencoded());
 
 // ROUTES
+// Home
 app.get("/home", (req, res) => {
   res.send(`<div> Hero </div>`);
 });
 
+// Projects
 app.get("/projects", (req, res) => {
   console.log(`REPO REQUEST RECEIVED`);
 
@@ -43,7 +53,7 @@ app.get("/projects", (req, res) => {
         ${repos
           .map(
             (repo) =>
-              `<li>${repo.name}, ${repo.language}, ${repo.url}, ${repo.created_at} </li>`
+              `<li>${repo.name}, ${repo.language}, ${repo.url}, ${repo.created_at}</li>`
           )
           .join("")}
       </ul>
@@ -57,64 +67,106 @@ app.get("/projects", (req, res) => {
   getRepos();
 });
 
+// About
 app.get("/about", (req, res) => {
-  res.send(`
-  <div class="flex flex-col">
-  <h1 class="pb-3">Daniel Holmes - Fullstack Engineer</h1>
-  <ul class="flex">
-    <li>
-      <h2>Frontend</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
-        molestias quam non deserunt atque inventore? Modi ducimus dolore placeat
-        reiciendis, accusamus eveniet alias obcaecati cum doloremque fuga, ut,
-        neque sunt!
-      </p>
-    </li>
+  res.send(about_template);
+});
 
-    <li>
-      <h2>Backend</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
-        molestias quam non deserunt atque inventore? Modi ducimus dolore placeat
-        reiciendis, accusamus eveniet alias obcaecati cum doloremque fuga, ut,
-        neque sunt!
-      </p>
-    </li>
+// CONTACT FORM
+app.get("/contact", (req, res) => {
+  res.send(contact_form);
+});
+app.post("/contact", (req, res) => {
+  const formUserInput: FormSchema = req.body;
+  res.send(form_submission_thankyou);
+});
 
-    <li>
-      <h2>Data Analysis and Database Management</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
-        molestias quam non deserunt atque inventore? Modi ducimus dolore placeat
-        reiciendis, accusamus eveniet alias obcaecati cum doloremque fuga, ut,
-        neque sunt!
-      </p>
-    </li>
+// Download Logic
+app.get("/downloadCv", (req, res) => {
+  res.download("./public/assets/filesToSend/SoftwareCV.pdf");
+});
 
-    <li>
-      <h2>Teaching Professional</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
-        molestias quam non deserunt atque inventore? Modi ducimus dolore placeat
-        reiciendis, accusamus eveniet alias obcaecati cum doloremque fuga, ut,
-        neque sunt!
-      </p>
-    </li>
-  </ul>
+// SERVER
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+// TEMPLATES
+const about_template = `
+<div class="flex flex-col">
+<h1 class="pb-3">Daniel Holmes - Fullstack Engineer</h1>
+<ul class="flex">
+  <li>
+    <h2>Frontend</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+      molestias quam non deserunt atque inventore? Modi ducimus dolore placeat
+      reiciendis, accusamus eveniet alias obcaecati cum doloremque fuga, ut,
+      neque sunt!
+    </p>
+  </li>
+
+  <li>
+    <h2>Backend</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+      molestias quam non deserunt atque inventore? Modi ducimus dolore placeat
+      reiciendis, accusamus eveniet alias obcaecati cum doloremque fuga, ut,
+      neque sunt!
+    </p>
+  </li>
+
+  <li>
+    <h2>Data Analysis and Database Management</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+      molestias quam non deserunt atque inventore? Modi ducimus dolore placeat
+      reiciendis, accusamus eveniet alias obcaecati cum doloremque fuga, ut,
+      neque sunt!
+    </p>
+  </li>
+
+  <li>
+    <h2>Teaching Professional</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+      molestias quam non deserunt atque inventore? Modi ducimus dolore placeat
+      reiciendis, accusamus eveniet alias obcaecati cum doloremque fuga, ut,
+      neque sunt!
+    </p>
+  </li>
+</ul>
+</div>
+`;
+
+const contact_form = `<form hx-post="/contact" hx-trigger="submit" hx-target="#display-content" hx-swap="beforeend">
+<div>
+  <label for="name">Name</label>
+  <input name="name" type="text" id="name" required />
 </div>
 
-   
-  `);
-});
-app.get("/contact", (req, res) => {
-  res.send(`<div> Contact </div>`);
-});
-app.get("/downloadCv", (req, res) => {
-  res.download("app/public/assets/filesToSend/SoftwareCV.pdf");
-});
+<div>
+  <label for="phone">Phone*</label>
+  <input name="phone" type="text" id="phone" />
+</div>
 
-// LISTEN
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+<div>
+  <label name="email">Email</label>
+  <input name="email" type="text" id="email" required />
+</div>
+
+<div>
+  <label for="comment">Comment</label>
+  <input
+    name="comment"
+    type="text"
+    id="comment"
+    placeholder="Please tell me a little bit about your request."
+  />
+</div>
+
+<button type="submit">Request More info</button>
+</form>
+`;
+
+const form_submission_thankyou = `<div>Thanks for contacting me. I will get back to you asap!</div>`;
