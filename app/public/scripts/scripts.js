60a240c5c9911ebfcd5c966e77e893562fe538ec
Daniel Holmes <senditholmes@gmail.com>
2024-07-30 15:34:21 +0900

    remove intersecting logic, basic styling

--- app/public/scripts/scripts.js
+++ app/public/scripts/scripts.js
@@ -1,13 +1,2 @@
-var aboutMeSection = document.querySelector("#aboutme-section");
-var scrollbar = document.querySelector("#scrollbar");
-let options = {
-    rootMargin: "0px",
-    threshold: 0.9,
-};
-var aboutMeObserver = new IntersectionObserver(handleIntersection, options);
-aboutMeObserver.observe(aboutMeSection);
-function handleIntersection(entries) {
-    return;
-}
 export {};
 //# sourceMappingURL=scripts.js.map
--- app/public/scripts/scripts.ts
+++ app/public/scripts/scripts.ts
@@ -1,19 +1,26 @@
-// animate in different sections
-// animate in the menu buttons at the top
-// animate in the repos
-// animate in the contact form
+// // DOM ELEMENTS
+// var aboutmeSection = document.querySelector("#aboutme");
+// var educatorSection = document.querySelector("#educator");
+// var teamUpSection = document.querySelector("#teamup");
 
-var aboutMeSection = document.querySelector("#aboutme-section");
-var scrollbar = document.querySelector("#scrollbar");
+// // INTERSECTION
+// let options = {
+//   rootMargin: "0px",
+//   threshold: 0.5,
+// };
+// var observer = new IntersectionObserver(handleIntersection, options);
 
-let options = {
-  rootMargin: "0px",
-  threshold: 0.9,
-};
+// observer.observe(aboutmeSection);
+// observer.observe(educatorSection);
+// observer.observe(teamUpSection);
 
-var aboutMeObserver = new IntersectionObserver(handleIntersection, options);
-aboutMeObserver.observe(aboutMeSection);
-
-function handleIntersection(entries: any) {
-  return;
-}
+// // HANDLERS
+// function handleIntersection(entries: IntersectionObserverEntry[]) {
+//   entries.forEach((entry: IntersectionObserverEntry) => {
+//     if (entry.isIntersecting) {
+//       console.log(`${entry.target.id} is intersecting`);
+//     } else {
+//       console.log(`${entry.target.id} is not intersecting`);
+//     }
+//   });
+// }
