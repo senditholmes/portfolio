a2f3d1d5f4cde654e7ce5bc8499a3cd720af9f27
Daniel Holmes <senditholmes@gmail.com>
2024-07-30 20:59:20 +0900

    add transform and transition styling to navbar list items

--- app/public/scripts/scripts.js
+++ app/public/scripts/scripts.js
@@ -1,2 +1,16 @@
+var aboutmeSection = document.querySelector("#nicetomeetyou");
+var educatorSection = document.querySelector("#educator");
+var teamUpSection = document.querySelector("#teamup");
+let options = {
+    rootMargin: "0px",
+    threshold: 0.5,
+};
+var observer = new IntersectionObserver(handleIntersection, options);
+observer.observe(aboutmeSection);
+observer.observe(educatorSection);
+observer.observe(teamUpSection);
+function handleIntersection(entries) {
+    entries.forEach((entry) => { });
+}
 export {};
 //# sourceMappingURL=scripts.js.map
--- app/public/scripts/scripts.ts
+++ app/public/scripts/scripts.ts
@@ -1,26 +1,20 @@
 // // DOM ELEMENTS
-// var aboutmeSection = document.querySelector("#aboutme");
-// var educatorSection = document.querySelector("#educator");
-// var teamUpSection = document.querySelector("#teamup");
+var aboutmeSection = document.querySelector("#nicetomeetyou");
+var educatorSection = document.querySelector("#educator");
+var teamUpSection = document.querySelector("#teamup");
 
 // // INTERSECTION
-// let options = {
-//   rootMargin: "0px",
-//   threshold: 0.5,
-// };
-// var observer = new IntersectionObserver(handleIntersection, options);
+let options = {
+  rootMargin: "0px",
+  threshold: 0.5,
+};
+var observer = new IntersectionObserver(handleIntersection, options);
 
-// observer.observe(aboutmeSection);
-// observer.observe(educatorSection);
-// observer.observe(teamUpSection);
+observer.observe(aboutmeSection);
+observer.observe(educatorSection);
+observer.observe(teamUpSection);
 
-// // HANDLERS
-// function handleIntersection(entries: IntersectionObserverEntry[]) {
-//   entries.forEach((entry: IntersectionObserverEntry) => {
-//     if (entry.isIntersecting) {
-//       console.log(`${entry.target.id} is intersecting`);
-//     } else {
-//       console.log(`${entry.target.id} is not intersecting`);
-//     }
-//   });
-// }
+// HANDLERS
+function handleIntersection(entries: IntersectionObserverEntry[]) {
+  entries.forEach((entry: IntersectionObserverEntry) => {});
+}
