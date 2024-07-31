306a37033866e894a694b9ba903de8eb6de077e2
Daniel Holmes <senditholmes@gmail.com>
2024-07-31 14:22:46 +0900

    landing page styling

--- app/public/scripts/scripts.ts
+++ app/public/scripts/scripts.ts
@@ -1,20 +1,20 @@
 // // DOM ELEMENTS
-var aboutmeSection = document.querySelector("#nicetomeetyou");
-var educatorSection = document.querySelector("#educator");
-var teamUpSection = document.querySelector("#teamup");
+// var aboutmeSection = document.querySelector("#nicetomeetyou");
+// var educatorSection = document.querySelector("#educator");
+// var teamUpSection = document.querySelector("#teamup");
 
-// // INTERSECTION
-let options = {
-  rootMargin: "0px",
-  threshold: 0.5,
-};
-var observer = new IntersectionObserver(handleIntersection, options);
+// // // INTERSECTION
+// let options = {
+//   rootMargin: "0px",
+//   threshold: 0.5,
+// };
+// var observer = new IntersectionObserver(handleIntersection, options);
 
-observer.observe(aboutmeSection);
-observer.observe(educatorSection);
-observer.observe(teamUpSection);
+// observer.observe(aboutmeSection);
+// observer.observe(educatorSection);
+// observer.observe(teamUpSection);
 
-// HANDLERS
-function handleIntersection(entries: IntersectionObserverEntry[]) {
-  entries.forEach((entry: IntersectionObserverEntry) => {});
-}
+// // HANDLERS
+// function handleIntersection(entries: IntersectionObserverEntry[]) {
+//   entries.forEach((entry: IntersectionObserverEntry) => {});
+// }
